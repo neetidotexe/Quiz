a2a09e58d6baf3594f7fb6d8890864cc346f6a6b
Neeti <neeti@playo.co>
2020-07-29 10:50:56 +0530

    Create UI for question one screen and show it

--- app/src/main/java/com/example/quiz/ui/HomeScreenFragment.kt
+++ app/src/main/java/com/example/quiz/ui/HomeScreenFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 
 import com.example.quiz.R
 
 class HomeScreenFragment : Fragment() {
 
+    lateinit var startImage: ImageView
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        val root = inflater.inflate(R.layout.fragment_home_screen, container, false)
+
+        startImage = root.findViewById(R.id.home_screen_start_image)
+
+        //click event for Start - navigate to question one
+        startImage.setOnClickListener {
+            showQuestionOne()
+        }
+
+        return root
+    }
+
+    fun showQuestionOne() {
+        findNavController().navigate(R.id.action_homeScreenFragment_to_questionOneFragment)
     }
 }
