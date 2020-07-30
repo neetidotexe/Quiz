b454e2a00ede7be867985bafa8ab00f74fc22620
Neeti <neeti@playo.co>
2020-07-31 04:57:23 +0530

    Create request object for calling api to post answers and get next question

--- app/src/main/java/com/example/quiz/entity/QuestionRequest.kt
+++ app/src/main/java/com/example/quiz/entity/QuestionRequest.kt
@@ -7,3 +7,11 @@ data class QuestionRequest(
     @SerializedName("user_id")
     val userId: String
 )
+
+data class NextQuestionRequest(
+    @SerializedName("user_id")
+    val userId: String,
+
+    @SerializedName("answers")
+    val currentAnswers: MutableList<Int>
+)
