cc64bbb0adb09214306eaac7c166416bec373692
Neeti <neeti@playo.co>
2020-07-31 06:13:53 +0530

    Update Question Two screen to accept multiple correct answers

--- app/src/main/java/com/example/quiz/ui/QuestionTwoFragment.kt
+++ app/src/main/java/com/example/quiz/ui/QuestionTwoFragment.kt
@@ -5,22 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.ImageView
-import android.widget.RadioButton
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-
 import com.example.quiz.R
 import com.example.quiz.UserId
 
 class QuestionTwoFragment : Fragment() {
 
     lateinit var questionText: TextView
-    lateinit var optionOne: RadioButton
-    lateinit var optionTwo: RadioButton
-    lateinit var optionThree: RadioButton
-    lateinit var optionFour: RadioButton
+    lateinit var optionOne: CheckBox
+    lateinit var optionTwo: CheckBox
+    lateinit var optionThree: CheckBox
+    lateinit var optionFour: CheckBox
     lateinit var nextQuestion: ImageView
     lateinit var viewModel: QuizViewModel
 
@@ -30,7 +29,7 @@ class QuestionTwoFragment : Fragment() {
     var optionFourId: Int? = null
 
     var passSelectedAnswersOfPreviousQuestion = mutableListOf<Int>()
-
+    private val selectedAnswersList = mutableListOf<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,10 +39,10 @@ class QuestionTwoFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_question_two, container, false)
 
         questionText = root.findViewById(R.id.question_two)
-        optionOne = root.findViewById(R.id.answer_two_radio_button_one)
-        optionTwo = root.findViewById(R.id.answer_two_radio_button_two)
-        optionThree = root.findViewById(R.id.answer_two_radio_button_three)
-        optionFour = root.findViewById(R.id.answer_two_radio_button_four)
+        optionOne = root.findViewById(R.id.answer_two_check_box_one)
+        optionTwo = root.findViewById(R.id.answer_two_check_box_two)
+        optionThree = root.findViewById(R.id.answer_two_check_box_three)
+        optionFour = root.findViewById(R.id.answer_two_check_box_four)
         nextQuestion = root.findViewById(R.id.question_two_screen_next_image)
         viewModel = ViewModelProvider(this).get(QuizViewModel::class.java)
 
